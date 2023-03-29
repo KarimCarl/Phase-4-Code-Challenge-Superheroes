b1134605123e89a34a3a952d03d32cfcb8b5b31b
KarimCarl <cndungu109@gmail.com>
2023-03-29 09:16:06 +0300

    Added controller methods to Hero Powers controller

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,16 +1,40 @@
 class PowersController < ApplicationController
 
-        def create
-        new_hero_power = HeroPower.create(allow_params)
-        if new_hero_power.valid?
-        render json: new_hero_power.hero,except:[:created_at,:updated_at], include:[powers:{except:[:created_at,:updated_at]}]
-        else
-            render json: {errors:["Validation errors"]}
-        end
+    def index
+        powers = Power.all
+        render json: powers,except:[:created_at,:updated_at]
         end
     
+        def show
+            begin
+            power = power_by_id
+            render json: power,except:[:created_at,:updated_at]
+            rescue ActiveRecord::RecordNotFound
+            render json:{error:"Power not found"},status: :not_found
+            end
+        end
+    
+        def update
+            begin
+            power = power_by_id
+            power.update(allow_params)
+            if power.valid?
+            render json: power, except:[:created_at,:updated_at],status:200
+            else
+            render json: {errors:["Validation errors"]},status:403
+            end
+            rescue ActiveRecord::RecordNotFound
+            render json:{error:"Power not found"},status: :not_found
+            end
+        end
+        
         private
+        
+        def power_by_id
+        Power.find(params[:id])
+        end
+    
         def allow_params
-        params.permit(:strength,:power_id,:hero_id)
+            params.permit(:description)
         end
 end
