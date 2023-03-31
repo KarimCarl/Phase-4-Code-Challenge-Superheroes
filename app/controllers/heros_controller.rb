a2b2229ce11cb0511ed3161d812efacd1eaa2d28
KarimCarl <cndungu109@gmail.com>
2023-03-31 07:12:23 +0300

    Modified controller

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,13 +1,13 @@
 class HerosController < ApplicationController
     def index
         heroes  = Hero.all
-        render json: heroes,except:[:created_at,:updated_at],status: :ok
+        render json: heroes, except:[:created_at,:updated_at],status: :ok
     end
 
     def show
         begin
         hero = Hero.find(params[:id])
-        render json: hero,except:[:created_at,:updated_at],include:[powers:{except:[:created_at,:updated_at]}]
+        render json: hero,except:[:created_at,:updated_at]
         rescue ActiveRecord::RecordNotFound
         render json:{error:"Hero not found"},status: :not_found
         end 
